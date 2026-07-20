00367e2f4e9991332aa7377dc931a9a03db5b7fd
mrSm00th <231942191+mrSm00th@users.noreply.github.com>
2026-07-20 20:13:29 +0530

    feat(db): add config settings and async database engine

---
+++ src/app/core/config.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        extra="ignore"
+    )
+
+    postgres_user: str
+    postgres_password: str
+    postgres_db: str
+    postgres_host: str = "localhost"
+    postgres_port: int = 5433
+
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.postgres_user}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
+    
+
+
+settings = Settings()
