8119164a2794e14b1759260b16e081bf0b485759
mrSm00th <231942191+mrSm00th@users.noreply.github.com>
2026-07-31 12:42:27 +0530

    feat(auth): add otp_verifications model to support email verifications using OTP's

--- src/app/modules/users/models.py
+++ src/app/modules/users/models.py
@@ -2,7 +2,7 @@ import enum
 import uuid
 from datetime import UTC, datetime
 
-from sqlalchemy import DateTime, Enum, String, Uuid, ForeignKey
+from sqlalchemy import DateTime, Enum, String, Uuid, ForeignKey, Boolean, Index, text
 from sqlalchemy.dialects.postgresql import INET
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -117,4 +117,59 @@ class RefreshToken(Base):
 
     user: Mapped["User"] = relationship(
         back_populates="refresh_tokens",
+    )
+
+
+class OTPVerification(Base):
+    __tablename__ = "otp_verifications"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        Uuid,
+        primary_key=True,
+        default=uuid.uuid4,
+    )
+
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        nullable=False,
+        index=True,
+    )
+
+    email: Mapped[str] = mapped_column(
+        String(120),
+        nullable=False,
+        index=True,
+    )
+
+    otp_hashed: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+    )
+
+    is_used: Mapped[bool] = mapped_column(
+        Boolean,
+        nullable=False,
+        default=False,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+        default=lambda: datetime.now(UTC),
+    )
+
+    expires_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        nullable=False,
+    )
+
+    user: Mapped[User] = relationship(back_populates="verification_otp")
+
+    __table_args__ = (
+        Index(
+            "idx_one_valid_otp_per_user",
+            "user_id",
+            unique=True,
+            postgresql_where=text("is_used=false"),
+        ),
     )
